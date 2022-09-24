a60842c2a3e7a0849f8266802ab205f1ccd19fa1
codusl100 <77263479+codusl100@users.noreply.github.com>
2022-09-25 05:04:37 +0900

    fix: vote swagger

--- src/main/java/cmc/changedworld/api/vote/VoteController.java
+++ src/main/java/cmc/changedworld/api/vote/VoteController.java
@@ -24,10 +24,7 @@ public class VoteController {
 
     private final VoteService voteService;
     @ApiOperation(value = "유저 투표", notes = "유저 투표입니다.")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId",value = "유저 Id"),
-            @ApiImplicitParam(name = "voteId",value = "투표 Id")
-    })
+    @ApiImplicitParam(name = "userId",value = "유저 Id", paramType = "path")
     @GetMapping("/x/{userId}")
     public BaseResponse<VoteResponseDto> getXCurrentVote(@PathVariable Long userId) {
         try {
@@ -37,6 +34,8 @@ public class VoteController {
         }
     }
 
+    @ApiOperation(value = "유저 투표", notes = "유저 투표입니다.")
+    @ApiImplicitParam(name = "userId",value = "유저 Id", paramType = "path")                  
     @GetMapping("/z/{userId}")
     public BaseResponse<VoteResponseDto> getZCurrentVote(@PathVariable Long userId) {
         try {
@@ -54,6 +53,8 @@ public class VoteController {
         return new BaseResponse<>(result);
     }
 
+    @ApiOperation(value = "유저 투표", notes = "유저 투표입니다.")
+    @ApiImplicitParam(name = "vostId",value = "투표 Id", paramType = "path")                  
     @PostMapping("/{voteId}/comment")
     public BaseResponse<Map<String, Long>> addVoteComment(
             @PathVariable Long voteId,
@@ -69,6 +70,8 @@ public class VoteController {
         }
     }
 
+    @ApiOperation(value = "유저 투표", notes = "유저 투표입니다.")
+    @ApiImplicitParam(name = "vostId",value = "투표 Id", paramType = "path")   
     @PostMapping("/{voteId}/check")
     public BaseResponse<VoteResponseDto> voteCheck(
             @PathVariable Long voteId,
