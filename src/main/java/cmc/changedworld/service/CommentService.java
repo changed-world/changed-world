9fa6bd25366573f53f180df188c88b8cec8fba40
Limjongho <whdgh9873@icloud.com>
2022-09-25 03:56:42 +0900

    dongmoo : 마이페이지 알림 수정

---
+++ src/main/java/cmc/changedworld/service/CommentService.java
@@ -0,0 +1,16 @@
+package cmc.changedworld.service;
+
+import cmc.changedworld.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CommentService {
+
+    private CommentRepository commentRepository;
+
+    public
+}
