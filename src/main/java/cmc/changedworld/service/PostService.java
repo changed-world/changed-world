c138038527a8c747f4d59e0c3be56bfc81be0f61
Limjongho <whdgh9873@icloud.com>
2022-09-24 22:29:22 +0900

    test commit

---
+++ src/main/java/cmc/changedworld/service/PostService.java
@@ -0,0 +1,13 @@
+package cmc.changedworld.service;
+
+import cmc.changedworld.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+}
