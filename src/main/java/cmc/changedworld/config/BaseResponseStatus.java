85f14f0444e58d176b65026ce3f54d59b502209e
wyoung163 <20201014@sungshin.ac.kr>
2022-09-24 23:45:59 +0900

    add exception

--- src/main/java/cmc/changedworld/config/BaseResponseStatus.java
+++ src/main/java/cmc/changedworld/config/BaseResponseStatus.java
@@ -4,9 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum BaseResponseStatus {
-    SUCCESS(true,200,"요청에 성공하였습니다.");
+    SUCCESS(true,200,"요청에 성공하였습니다."),
+    DATABASE_ERROR(false, 400, "데이터베이스 연결에 실패하였습니다."),
 
-
+    //Empathy 중복 확인
+    POST_EMPATHY_INVALID(false, 400, "해당 게시글에 대한 반성해요를 이미 선택하였습니다.");
 
 
     private final boolean isSuccess;
