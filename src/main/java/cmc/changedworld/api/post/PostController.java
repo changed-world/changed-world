6a2b937aa6ac3caaa00ffc8029fbe61e875574e5
wyoung163 <20201014@sungshin.ac.kr>
2022-09-25 04:15:32 +0900

    add: post swagger

--- src/main/java/cmc/changedworld/api/post/PostController.java
+++ src/main/java/cmc/changedworld/api/post/PostController.java
@@ -7,6 +7,9 @@ import cmc.changedworld.config.BaseException;
 import cmc.changedworld.config.BaseResponse;
 import cmc.changedworld.domain.PostType;
 import cmc.changedworld.service.PostService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +21,10 @@ import java.util.List;
 public class PostController {
     private final PostService postService;
 
+    @ApiOperation(value = "게시글 종류 별 게시글 목록 조회", notes = "XtoX, XtoY와 같은 게시글 종류 별로 게시글 목록을 조회합니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "type", value = "게시글 종류", required = true, dataType = "String", paramType = "query")
+    })
     @GetMapping("")
     public BaseResponse<List<GetPostRes>> getPostListByType(@RequestParam String type) {
         try {
@@ -27,16 +34,26 @@ public class PostController {
         }
     }
 
+    @ApiOperation(value = "게시글 아이디로 게시글 조회", notes = "게시글 아이디에 해당하는 게시글을 조회합니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "postId", value = "게시글 아이디", required = true, dataType = "Long", paramType = "path")
+    })
     @GetMapping("/{postId}")
-    public BaseResponse<GetPostRes> getPostListByPostId(@PathVariable Long postId) {
+    public BaseResponse<GetPostRes> getPostByPostId(@PathVariable Long postId) {
         try {
-            return new BaseResponse<>(postService.getPostListByPostId(postId));
+            return new BaseResponse<>(postService.getPostByPostId(postId));
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus());
         }
     }
 
-
+    @ApiOperation(value = "게시글 작성", notes = "게시글을 작성합니다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userId", value = "사용자 아이디", required = true, dataType = "Long", paramType = "body"),
+            @ApiImplicitParam(name = "title", value = "게시글 제목", required = true, dataType = "String", paramType = "body"),
+            @ApiImplicitParam(name = "content", value = "게시글 내용", required = true, dataType = "String", paramType = "body"),
+            @ApiImplicitParam(name = "postType", value = "게시글 종류", required = true, dataType = "String", paramType = "body")
+    })
     @PostMapping("")
     public BaseResponse<PostPostRes> createPost(@RequestBody PostPostReq postPostReq) {
 
